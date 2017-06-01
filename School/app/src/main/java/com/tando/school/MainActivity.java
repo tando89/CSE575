066b39ab0f7b8e86d32337380f3dca4e10d65000
Tan Do <tando@Tans-MacBook-Pro.local>
2017-06-01 00:38:39 -0700

    Finished the preference activity

--- School/app/src/main/java/com/tando/school/MainActivity.java
+++ School/app/src/main/java/com/tando/school/MainActivity.java
@@ -1,8 +1,10 @@
 package com.tando.school;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,5 +22,21 @@ public class MainActivity extends AppCompatActivity {
         getMenuInflater().inflate(R.menu.settingicon, menu);
         return super.onCreateOptionsMenu(menu);
     }
+    //Called a boolean called onOptionsItemSelected
 
+    @Override
+    //Called when a panel's menu is opened by the user
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId())
+        {
+            //case that the user clicks on the setting icon
+            case R.id.settingsIcon:
+                //when the user clicks, it open a loadPreferenceActivity
+                Intent intent = new Intent(this, loadPreferenceActivity.class);
+                startActivity(intent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
 }
