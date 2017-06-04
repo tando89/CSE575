b2d909fc22cd72512daed6d95ad8abc1d51bd3f2
tando89 <tantdo89@gmail.com>
2017-06-03 21:00:48 -0700

    finished section1

--- School/app/src/main/java/com/tando/school/MainActivity.java
+++ School/app/src/main/java/com/tando/school/MainActivity.java
@@ -1,10 +1,12 @@
 package com.tando.school;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -38,5 +40,35 @@ public class MainActivity extends AppCompatActivity {
             default:
                 return super.onOptionsItemSelected(item);
         }
+    }
+    //link to FaceBook
+    public void fbClick (View view) {
+        //use startActivity and Intent methods to parse the link
+        startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.facebook.com/flitetest/")));
+    }
+    //google+
+    public void googleClick (View view) {
+        //use startActivity and Intent methods to parse the link
+        startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://plus.google.com/u/0/+google")));
+    }
+    //LinkedIn
+    public void linkedInClick (View view) {
+        //use startActivity and Intent methods to parse the link
+        startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.linkedin.com/in/mbacsusb/")));
+    }
+    //youtube
+    public void youTubeClick (View view) {
+        //use startActivity and Intent methods to parse the link
+        startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.youtube.com/channel/UCwqJShoifP6032UXYe-sD-A")));
+    }
+    //Twitter
+    public void twitterClick (View view) {
+        //use startActivity and Intent methods to parse the link
+        startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://twitter.com/Google")));
+    }
+
+    //MySchoolAccount
+    public void StartAccount (View v) {
+        startActivity(new Intent(this, MySchoolAccount.class));
     }
 }
